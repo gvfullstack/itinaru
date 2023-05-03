214d618225325a7178edc41f2ed0d5ae6e2ca19b
Gabriel <lsvillegas7@yahoo.com>
2023-05-03 08:58:45 -0700

    acfd

--- src/components/searchMap.tsx
+++ src/components/searchMap.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
+// import React from 'react';
 
-const myAPIKEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
+// const myAPIKEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
 
-const center = {
-    lat: 37.3688,
-    lng: -122.0363,
-  };
+// const center = {
+//     lat: 37.3688,
+//     lng: -122.0363,
+//   };
 
-const SearchMap = ({ q }) => {
+// const SearchMap = ({ q }) => {
 
-  const src = `https://www.google.com/maps/embed/v1/search?key=${myAPIKEY}&q=historical+sites+in+sunnyvale`;
+//   const src = `https://www.google.com/maps/embed/v1/search?key=${myAPIKEY}&q=historical+sites+in+sunnyvale`;
 
-  return (
-    <div>
-    <iframe
-      width="90%"
-      height="500rem"
-      frameborder="0"
-      style={{ border: 20 }}
-      referrerpolicy="no-referrer-when-downgrade"
-      src={src}
-      allowfullscreen
-      center={center}
-    //   source="outdoor"
-    />
+//   return (
+//     <div>
+//     <iframe
+//       width="90%"
+//       height="500rem"
+//       frameborder="0"
+//       style={{ border: 20 }}
+//       referrerpolicy="no-referrer-when-downgrade"
+//       src={src}
+//       allowfullscreen
+//       center={center}
+//     //   source="outdoor"
+//     />
 
-    <iframe
-    width="90%"
-    height="500rem"
-    frameborder="0"
-    style={{ border: 20 }}
-    referrerpolicy="no-referrer-when-downgrade"
-    src={src}
-    allowfullscreen
-    center={center}
-  //   source="outdoor"
-  />
-  </div>
-  );
-};
+//     <iframe
+//     width="90%"
+//     height="500rem"
+//     frameborder="0"
+//     style={{ border: 20 }}
+//     referrerpolicy="no-referrer-when-downgrade"
+//     src={src}
+//     allowfullscreen
+//     center={center}
+//   //   source="outdoor"
+//   />
+//   </div>
+//   );
+// };
 
-export default SearchMap;
+// export default SearchMap;
