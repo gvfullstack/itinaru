289cef40bfedb10083db73e010750eb8906579bf
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2023-05-04 20:05:05 -0700

    config

--- src/pages/api/itinaru/trip.tsx
+++ src/pages/api/itinaru/trip.tsx
@@ -3,9 +3,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import cache from 'memory-cache';
 import rateLimit from 'express-rate-limit';
 
-// export const config = {
-//   runtime: "edge"
-// };
+export const config = {
+  runtime: "edge"
+};
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -202,6 +202,7 @@ async function requestItineraryFunction(
             ],
         temperature: 0.7,
         max_tokens: 2048,
+        stream: true,
 
     });
 
