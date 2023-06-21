8a88427fccd92f20dfa5f70a19487f06a77a4861
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2023-06-20 18:45:43 -0700

    add city list

--- src/components/FormComponentsTravelPreferences/tpDestinationInput.tsx
+++ src/components/FormComponentsTravelPreferences/tpDestinationInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Box } from "@mui/material";
 import { styled } from '@mui/material/styles';
 import Autocomplete from '@mui/material/Autocomplete';
@@ -18,43 +18,114 @@ const PinkOutlinedTextField = styled(TextField)(({ theme }) => ({
     borderColor: 'pink',
   },
   '& .MuiOutlinedInput-input': {
-    fontSize: '18px',
+    fontSize: '1rem',
     fontWeight: '400',
-    padding: '10px 10px 10px 20px' ,
+    padding: '10px 10px 10px 20px',
   },
   width: "90%",
   maxWidth: '20rem',
 }));
 
+interface City {
+  city: string;
+}
+
 function DestinationInput() {
   const [tripPreferences, setTripPreferencesAtom] = useRecoilState(tripPreferencesAtom);
-  const selectedCity = cities.find(city => `${city.city}, ${city.state}` === tripPreferences.destination) || null;
+  const selectedCity = cities.find(city => `${city.city}` === tripPreferences.destination) || null;
   const maxLength = 255;
+  const debounceDelay = 5000; // Adjust the debounce delay as per your preference
+  const suggestionsDelay = 3000; // Delay before showing suggestions in milliseconds
 
-  const handleChange = (_: any, newValue: { city: string; state: string } | null) => {
-    setTripPreferencesAtom((prevTripPreferenceState)=> ({...prevTripPreferenceState, destination: newValue ? `${newValue.city}, ${newValue.state}` : ''}));
-  }
+  const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState<City[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(false); // Track whether to show suggestions
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowSuggestions(inputValue !== '' && suggestions.length > 0);
+    }, suggestionsDelay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [inputValue, suggestions, suggestionsDelay]);
+
+  // Create a debounced version of the search function
+  const debouncedSearch = debounce((value: string) => {
+    // Perform the actual search operation here and update the suggestions state
+    const filteredOptions = cities.filter((city) => {
+      const cityName = city.city;
+      if (typeof cityName === 'string') {
+        return cityName.toLowerCase().includes(value.toLowerCase());
+      }
+      return false;
+    });
+    setSuggestions(filteredOptions);
+  }, debounceDelay);
+
+  const handleChange = (event: React.ChangeEvent<{}>, value: { city: string } | null) => {
+    const destination = value ? value.city : '';
+    setInputValue(destination);
+    setTripPreferencesAtom((prevTripPreferenceState) => ({
+      ...prevTripPreferenceState,
+      destination: destination
+    }));
+  };
+
+  const handleInputChange = (event: React.SyntheticEvent<Element, Event>, value: string) => {
+    const inputValue = value || '';
+    setInputValue(inputValue);
+    debouncedSearch(inputValue);
+  };
+
+  const handleInputBlur = () => {
+    if (inputValue === '') {
+      setInputValue('');
+      setSuggestions([]);
+    }
+  };
+
+  const filterOptions = () => {
+    return suggestions;
+  };
+
+  
   return (
     <Box display="flex" justifyContent="center">
       <Autocomplete
-        options={cities}
-        getOptionLabel={(option) => `${option.city}, ${option.state}`}
+        options={filterOptions()}
+        getOptionLabel={(option) => option.city}
         style={{ width: 500 }}
         value={selectedCity}
         onChange={handleChange}
-        renderInput={(params) => 
+        inputValue={inputValue}
+        onInputChange={handleInputChange}
+        onBlur={handleInputBlur}
+        renderInput={(params) =>
           <PinkOutlinedTextField
             {...params}
             label="Enter your destination"
             variant="outlined"
             size="small"
             disabled={!!tripPreferences.destination && tripPreferences.destination.length >= maxLength}
-            />
+          />
         }
+        loading={!showSuggestions} // Show loading indicator when suggestions are being fetched
+        loadingText="Loading..."
+        noOptionsText="No matching cities"
       />
     </Box>
   );
-};
+}
+
+// Debounce function implementation
+function debounce<T extends (...args: any[]) => void>(func: T, delay: number) {
+  let timeoutId: NodeJS.Timeout;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  } as T;
+}
 
 export default DestinationInput;
