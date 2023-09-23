d8e939fd0206040b95f5627cea83ec1472fb0df6
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2023-09-23 03:03:23 -0700

    formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ return (
       <Head>
         <title>itinaru</title>
         <meta name="description" content="Itinerary builder." />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
        </Head>
       
 
