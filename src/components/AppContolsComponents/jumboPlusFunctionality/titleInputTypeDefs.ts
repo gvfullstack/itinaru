a463235888e54f3691e5d786141045c105e97c6f
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2023-10-07 15:33:41 -0700

    added my itineraries section

--- src/components/AppContolsComponents/jumboPlusFunctionality/titleInputTypeDefs.ts
+++ src/components/AppContolsComponents/jumboPlusFunctionality/titleInputTypeDefs.ts
@@ -3,78 +3,47 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 export type ItineraryItem = {
-    siteName?: string;
-    startTime?: {time?: Dayjs | null};
-    endTime?: {time?: Dayjs | null};
-    description?: string;
-    location?: {latitude: number, longitude: number};
-    locationAddress?: string;
-    rating?: number;
-    locationWebsite?: string;
-    expectedPerPersonBudget?: string;
-    descHidden?: boolean;
-    id?: string;
-    averageWeatherOnTravelDate?: string;
-    activityDuration?: number;
-    userDefinedRespectedTime?: boolean;
-    activityType?: string;
-    itineraryParentId?: string;
-    beingEdited?: boolean
-  } 
+  siteName?: string;
+  startTime?: {time?: Dayjs | null} ;
+  endTime?: {time?: Dayjs | null} ;
+  description?: string;
+  location?: {latitude: number, longitude: number};
+  locationAddress?: string;
+  rating?: number;
+  locationWebsite?: string;
+  expectedPerPersonBudget?: string;
+  descHidden?: boolean;
+  id?: string;
+  averageWeatherOnTravelDate?: string;
+  activityDuration?: number;
+  userDefinedRespectedTime?: boolean;
+  activityType?: string;
+  itineraryParentId?: string;
+  beingEdited?: boolean
+} 
 
-  export const ItemTypes = {
-    ITINERARY_ITEM: 'itineraryItem',
-  };
-  
-  export type ItineraryItems = ItineraryItem[];
+export const ItemTypes = {
+  ITINERARY_ITEM: 'itineraryItem',
+};
 
-  export type ItinerarySettings = {
-    title: string;
-    description: string;
-    neighborhood?: string;
-    city: string;
-    state: string;
-    duration?: string;
-    galleryPhotoUrl?: string;
-    visibility: 'private' | 'shared' | 'public';
-    readAccess?: string[];
-    editAccess?: string[];
-  }
-      
-  export type Itinerary = {
-    id?: string,
-    uid: string,
-    settings: ItinerarySettings;
-    items: ItineraryItems;
-  }
+export type ItineraryItems = ItineraryItem[];
 
-
-  export type TimeObject = {
-    time?: firebase.firestore.Timestamp | null;
-  };
-  
-  export type TransformedItineraryItem   = {
-    siteName?: string;
-    startTime?: TimeObject;
-    endTime?: TimeObject;
-    description?: string;
-    location?: {latitude: number, longitude: number};
-    locationAddress?: string;
-    rating?: number;
-    locationWebsite?: string;
-    expectedPerPersonBudget?: string;
-    descHidden?: boolean;
-    id?: string;
-    averageWeatherOnTravelDate?: string;
-    activityDuration?: number;
-    userDefinedRespectedTime?: boolean;
-    activityType?: string;
-    itineraryParentId?: string;
-    beingEdited?: boolean
-  } 
-
- export type TransformedItinerary = {
-    id?: string;
-    settings: ItinerarySettings;
-    items: TransformedItineraryItem[];
+export type ItinerarySettings = {
+  title: string;
+  description: string;
+  neighborhood?: string;
+  city: string;
+  state: string;
+  duration?: string;
+  galleryPhotoUrl?: string;
+  visibility: 'private' | 'shared' | 'public';
+  readAccess?: string[];
+  editAccess?: string[];
+}
+    
+export type Itinerary = {
+  id?: string,
+  uid?: string,
+  settings?: ItinerarySettings;
+  items?: ItineraryItems;
 }
