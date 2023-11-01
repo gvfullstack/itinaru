14d43a7231c46d63792d8174f1e87d6278adcf33
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2023-10-31 21:01:48 -0700

    formatting updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,7 +47,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <div style={{ width:"100%" }}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
 
       <Script
