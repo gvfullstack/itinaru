6a44d1a9c3dbea9748381d647b1474a67e044522
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2023-12-23 13:00:21 -0800

    sitemap

--- src/server/sitemap/getDataForSitemap.ts
+++ src/server/sitemap/getDataForSitemap.ts
@@ -1,25 +1,25 @@
-// import admin from 'firebase-admin';
-// import dbServer from '../../utils/firebase.admin';
-// import {TransformedItinerary} from '../../components/EditFormComponents/editFormTypeDefs';
+import admin from 'firebase-admin';
+import dbServer from '../../utils/firebase.admin';
+import {TransformedItinerary} from '../../components/EditFormComponents/editFormTypeDefs';
 
-// type SitemapItinerary = {
-//     id: string;
-//     // Add any other properties you might need for the sitemap
-//   };
+type SitemapItinerary = {
+    id: string;
+    // Add any other properties you might need for the sitemap
+  };
 
-// async function fetchItineraries(): Promise<SitemapItinerary[]> {
-//     const itinerariesRef = dbServer.collection('itineraries');
-//     const snapshot = await itinerariesRef.where('settings.visibility', '==', 'public').where('isDeleted', '==', false).get();
+async function fetchItineraries(): Promise<SitemapItinerary[]> {
+    const itinerariesRef = dbServer.collection('itineraries');
+    const snapshot = await itinerariesRef.where('settings.visibility', '==', 'public').where('isDeleted', '==', false).get();
   
-//     const itineraries: SitemapItinerary[] = [];
-//     snapshot.forEach(doc => {
-//       const itineraryData = doc.data();
-//       // Assuming 'id' is a direct property of the document
-//       if (itineraryData.id && !itineraryData.isDeleted) {
-//         itineraries.push({ id: itineraryData.id });
-//       }
-//     });
+    const itineraries: SitemapItinerary[] = [];
+    snapshot.forEach(doc => {
+      const itineraryData = doc.data();
+      // Assuming 'id' is a direct property of the document
+      if (itineraryData.id && !itineraryData.isDeleted) {
+        itineraries.push({ id: itineraryData.id });
+      }
+    });
   
-//     return itineraries;
-//   }
+    return itineraries;
+  }
   
