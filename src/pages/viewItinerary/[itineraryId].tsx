5d875943a9a3133258bf055c5676e049826e7bee
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2023-12-22 16:02:16 -0800

    non-logged in user fix

--- src/pages/viewItinerary/[itineraryId].tsx
+++ src/pages/viewItinerary/[itineraryId].tsx
@@ -14,14 +14,17 @@ export const getServerSideProps = async (context:GetServerSidePropsContext) => {
   const itineraryId = context.params?.itineraryId as string; // Extracting itineraryId from context
   const idToken = context.req.cookies['idToken']; // Replace with your cookie name
 
-  if (!idToken) {
-    // Handle the case where the token is not present
-    return { props: { itinerary: null } };
-  }
 
    try {
       const decodedToken = await authServer.verifyIdToken(idToken);
-      const userId = decodedToken.uid;
+
+      let  userId;
+
+      if (!idToken) {
+        userId = "unknownUser";
+      }else{
+        userId = decodedToken.uid;
+      }
 
       const itineraryData = await fetchItineraryFromDatabase(itineraryId, userId);
 
