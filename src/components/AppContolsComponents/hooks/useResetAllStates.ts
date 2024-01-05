d3cae28f3527a0585e13b0480850c70b577a2216
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2024-01-05 10:59:09 -0800

    added timestamps and bug fixes"

--- src/components/AppContolsComponents/hooks/useResetAllStates.ts
+++ src/components/AppContolsComponents/hooks/useResetAllStates.ts
@@ -34,28 +34,80 @@ import {currentlyViewingItineraryState} from '../../PublicItineraryViewComponent
 //search bar results atoms
 import {searchResultsState, searchQueryState} from '../../SearchBar/searchAtoms';
 
-const useResetAllStates = () => {
-    const atomsToReset = [
-        authUserState, privacySettingsState, 
-        itineraryItemsState, curStepState, 
-        defaultAtom, affiliatesAtom, 
-        userPreferencesAtom, tripPreferencesAtom, 
-        neighborhoodRecommendationList, pagePropsAtom,
-        currentlyEditingItineraryState, showItineraryEditForm, 
-        saveStatusDisplayedEditFormContainer, searchUserResultsState, 
-        searchUserQueryState, itineraryAccess, 
-        itineraryAccessItinView, itineraryInEditNeedsDeletionFromRecoilState,
-        myItinerariesResults, currentlyViewingItineraryState,
-        searchResultsState, searchQueryState  
-    ];
 
-   // Create a reset function for each atom
-    const resets = atomsToReset.map(atom => useResetRecoilState(atom));
+const useResetAuthUserState = () => useResetRecoilState(authUserState);
+const useResetPrivacySettingsState = () => useResetRecoilState(privacySettingsState);
+const useResetItineraryItemsState = () => useResetRecoilState(itineraryItemsState);
+const useResetCurStepState = () => useResetRecoilState(curStepState);
+const useResetDefaultAtom = () => useResetRecoilState(defaultAtom);
+const useResetAffiliatesAtom = () => useResetRecoilState(affiliatesAtom);
+const useResetUserPreferencesAtom = () => useResetRecoilState(userPreferencesAtom);
+const useResetTripPreferencesAtom = () => useResetRecoilState(tripPreferencesAtom);
+const useResetNeighborhoodRecommendationList = () => useResetRecoilState(neighborhoodRecommendationList);
+const useResetPagePropsAtom = () => useResetRecoilState(pagePropsAtom);
+const useResetCurrentlyEditingItineraryState = () => useResetRecoilState(currentlyEditingItineraryState);
+const useResetShowItineraryEditForm = () => useResetRecoilState(showItineraryEditForm);
+const useResetSaveStatusDisplayedEditFormContainer = () => useResetRecoilState(saveStatusDisplayedEditFormContainer);
+const useResetSearchUserResultsState = () => useResetRecoilState(searchUserResultsState);
+const useResetSearchUserQueryState = () => useResetRecoilState(searchUserQueryState);
+const useResetItineraryAccess = () => useResetRecoilState(itineraryAccess);
+const useResetItineraryAccessItinView = () => useResetRecoilState(itineraryAccessItinView);
+const useResetItineraryInEditNeedsDeletionFromRecoilState = () => useResetRecoilState(itineraryInEditNeedsDeletionFromRecoilState);
+const useResetMyItinerariesResults = () => useResetRecoilState(myItinerariesResults);
+const useResetCurrentlyViewingItineraryState = () => useResetRecoilState(currentlyViewingItineraryState);
+const useResetSearchResultsState = () => useResetRecoilState(searchResultsState);
+const useResetSearchQueryState = () => useResetRecoilState(searchQueryState);
+
+
+const useResetAllStates = () => {
+  const resetAuthUserState = useResetAuthUserState();
+  const resetPrivacySettingsState = useResetPrivacySettingsState();
+  const resetItineraryItemsState = useResetItineraryItemsState();
+  const resetCurStepState = useResetCurStepState();
+  const resetDefaultAtom = useResetDefaultAtom();
+  const resetAffiliatesAtom = useResetAffiliatesAtom();
+  const resetUserPreferencesAtom = useResetUserPreferencesAtom();
+  const resetTripPreferencesAtom = useResetTripPreferencesAtom();
+  const resetNeighborhoodRecommendationList = useResetNeighborhoodRecommendationList();
+  const resetPagePropsAtom = useResetPagePropsAtom();
+  const resetCurrentlyEditingItineraryState = useResetCurrentlyEditingItineraryState();
+  const resetShowItineraryEditForm = useResetShowItineraryEditForm();
+  const resetSaveStatusDisplayedEditFormContainer = useResetSaveStatusDisplayedEditFormContainer();
+  const resetSearchUserResultsState = useResetSearchUserResultsState();
+  const resetSearchUserQueryState = useResetSearchUserQueryState();
+  const resetItineraryAccess = useResetItineraryAccess();
+  const resetItineraryAccessItinView = useResetItineraryAccessItinView();
+  const resetItineraryInEditNeedsDeletionFromRecoilState = useResetItineraryInEditNeedsDeletionFromRecoilState();
+  const resetMyItinerariesResults = useResetMyItinerariesResults();
+  const resetCurrentlyViewingItineraryState = useResetCurrentlyViewingItineraryState();
+  const resetSearchResultsState = useResetSearchResultsState();
+  const resetSearchQueryState = useResetSearchQueryState();
   
-  // Return a function that when called, will reset all states
   return () => {
-    resets.forEach(reset => reset());
-  };
+    resetAuthUserState();
+    resetPrivacySettingsState();
+    resetItineraryItemsState();
+    resetCurStepState();
+    resetDefaultAtom();
+    resetAffiliatesAtom();
+    resetUserPreferencesAtom();
+    resetTripPreferencesAtom();
+    resetNeighborhoodRecommendationList();
+    resetPagePropsAtom();
+    resetCurrentlyEditingItineraryState();
+    resetShowItineraryEditForm();
+    resetSaveStatusDisplayedEditFormContainer();
+    resetSearchUserResultsState();
+    resetSearchUserQueryState();
+    resetItineraryAccess();
+    resetItineraryAccessItinView();
+    resetItineraryInEditNeedsDeletionFromRecoilState();
+    resetMyItinerariesResults();
+    resetCurrentlyViewingItineraryState();
+    resetSearchResultsState();
+    resetSearchQueryState();    
+  }
+
 };
 
 export default useResetAllStates;
