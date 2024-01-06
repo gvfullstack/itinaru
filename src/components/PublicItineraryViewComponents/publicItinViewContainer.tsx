043fce40ee00bc9e02d399c39396d9e3c0603c7e
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2024-01-05 18:39:38 -0800

    added timestamps and bug fixes"

--- src/components/PublicItineraryViewComponents/publicItinViewContainer.tsx
+++ src/components/PublicItineraryViewComponents/publicItinViewContainer.tsx
@@ -6,11 +6,9 @@ import {useRecoilState} from 'recoil';
 import styles from './publicItineraryView.module.css'
 import GoogleMapIframe from './directionsMapPV';
 import NotLoggedInModal from './copyItineraryUtilityFunctions/notLoggedInModal';
-import {TimeObject} from './publicItinViewTypeDefs'
 import {currentlyViewingItineraryState} from './publicItinViewAtoms';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
-
 
 const GeneralItineraryInformation = dynamic(() => 
     import('./generalItineraryInformationSection'), {
@@ -32,7 +30,7 @@ const PublicItinViewContainer: FC = () => {
   const [showNotLoggedInModalState, setShowNotLoggedInModalState] = useRecoilState(showNotLoggedInModal);
   const [itinerary, setItinerary] = useRecoilState(currentlyViewingItineraryState);
  
-  function formatTimestamp(timestamp: firebase.firestore.Timestamp | Date | firebase.firestore.FieldValue | null | undefined): string {
+  function formatTimestamp(timestamp: firebase.firestore.Timestamp | Date | { seconds: number, nanoseconds: number } | firebase.firestore.FieldValue | null | undefined): string {
     if (!timestamp) return '';
   
     if (timestamp instanceof Date) {
@@ -41,6 +39,10 @@ const PublicItinViewContainer: FC = () => {
     } else if (timestamp instanceof firebase.firestore.Timestamp) {
       // Firestore Timestamp object
       return timestamp.toDate().toLocaleString();
+    } else if ('seconds' in timestamp && 'nanoseconds' in timestamp) {
+      // Object with seconds and nanoseconds (similar to Firestore Timestamp)
+      const date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
+      return date.toLocaleString();
     } else if (timestamp instanceof firebase.firestore.FieldValue) {
       // Firestore FieldValue (e.g., serverTimestamp) - not a readable timestamp in client-side code
       return 'Pending...'; // or some placeholder text, as actual timestamp is not available yet
@@ -48,7 +50,7 @@ const PublicItinViewContainer: FC = () => {
   
     return ''; // Fallback for null, undefined, or unrecognized format
   }
-      
+       
 
 return (
 <div className={styles.publicItinViewContainer}>
