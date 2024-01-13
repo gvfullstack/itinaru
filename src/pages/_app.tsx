be0f50b53e2592799d0a5c6a0fb18d7bce54216c
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2024-01-13 13:11:00 -0800

    wording revision

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,6 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div style={{ width:"100vw", backgroundColor:"red"}}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+        <meta name="mobile-web-app-capable" content="yes"></meta>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
