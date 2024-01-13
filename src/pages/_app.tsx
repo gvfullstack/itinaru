2bcdd42bbc5ade2a780f606b8035e0cd00d47752
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2024-01-13 13:05:37 -0800

    wording revision

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,9 +27,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
   return (
-    <div style={{ width:"100%" }}>
+    <div style={{ width:"100vw", backgroundColor:"red"}}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
@@ -42,6 +42,7 @@ export default function App({ Component, pageProps }: AppProps) {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-WFW7FT84');`,
           }}
+          
         />
 
       </Head>
